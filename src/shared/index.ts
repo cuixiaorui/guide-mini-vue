ad326d009d51a4ec15a74381f8fdd584d3894962
qinran0423 <1156107187@qq.com>
2021-09-16 23:25:04 +0800

    修改hasChanged 参数

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -4,6 +4,6 @@ export const isObject = (value) => {
   return value !== null && typeof value === "object";
 };
 
-export const hasChanged = (val, newValue) => {
-  return !Object.is(val, newValue);
+export const hasChanged = (newValue, val) => {
+  return !Object.is(newValue, val);
 };
