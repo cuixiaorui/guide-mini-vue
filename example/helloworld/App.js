a853c31dbf56e04cbe46d2cd9416f2d0deb017a9
cuixiaorui <cui_xiaorui@126.com>
2021-10-03 15:02:34 +0800

    实现初始化 element 主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -4,7 +4,18 @@ export const App = {
   // 必须要写 render
   render() {
     // ui
-    return h("div", "hi, " + this.msg);
+    return h(
+      "div",
+      {
+        id: "root",
+        class: ["red", "hard"],
+      },
+      // "hi, " + this.msg
+      // string
+      // "hi, mini-vue"
+      // Array
+      [h("p", { class:"red"}, "hi"), h("p", {class:"blue"}, "mini-vue")]
+    );
   },
 
   setup() {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
@@ -5,12 +6,43 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-  // TODO 判断vnode 是不是一个 element
-  // 是 element 那么就应该处理 element
-  // 思考题： 如何去区分是 element 还是 component 类型呢？
-  // processElement();
+  if (typeof vnode.type === "string") {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container);
+  }
+}
 
-  processComponent(vnode, container);
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode: any, container: any) {
+  const el = document.createElement(vnode.type);
+
+  const { children } = vnode;
+
+  // children
+  if (typeof children === "string") {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el);
+  }
+
+  // props
+  const { props } = vnode;
+  for (const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+
+  container.append(el);
+}
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach((v) => {
+    patch(v, container);
+  });
 }
 
 function processComponent(vnode: any, container: any) {
