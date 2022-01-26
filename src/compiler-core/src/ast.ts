3b8c6cb7b399d7f1f5c87c6306f8a0c4164c8497
cuixiaorui <cui_xiaorui@126.com>
2022-01-26 21:24:35 +0800

    实现解析 element 标签

--- src/compiler-core/src/ast.ts
+++ src/compiler-core/src/ast.ts
@@ -1,4 +1,5 @@
 export const enum NodeTypes {
   INTERPOLATION,
   SIMPLE_EXPRESSION,
+  ELEMENT
 }
--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast";
 
+const enum TagType {
+  Start,
+  End,
+}
+
 export function baseParse(content: string) {
   const context = createParserContext(content);
   return createRoot(parseChildren(context));
@@ -9,13 +14,41 @@ function parseChildren(context) {
   const nodes: any = [];
 
   let node;
-  if (context.source.startsWith("{{")) {
-     node = parseInterpolation(context);
+  const s = context.source;
+  if (s.startsWith("{{")) {
+    node = parseInterpolation(context);
+  } else if (s[0] === "<") {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
   }
 
   nodes.push(node);
 
   return nodes;
+}
+
+function parseElement(context: any) {
+  const element = parseTag(context, TagType.Start);
+
+  parseTag(context, TagType.End);
+
+  return element;
+}
+
+function parseTag(context: any, type: TagType) {
+  // <div></div>
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+
+  if (type === TagType.End) return;
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
 }
 
 function parseInterpolation(context) {
@@ -34,7 +67,7 @@ function parseInterpolation(context) {
   const rawContentLength = closeIndex - openDelimiter.length;
 
   const rawContent = context.source.slice(0, rawContentLength);
-  const content = rawContent.trim()
+  const content = rawContent.trim();
 
   advanceBy(context, rawContentLength + closeDelimiter.length);
 
